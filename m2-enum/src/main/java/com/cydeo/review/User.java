f51b7bbd14b8b0a376b207e60c27199cce737ad2
CundullahT-CT <cterzioglu@cybertekschool.com>
2023-01-29 19:03:48 +0100

    enums review

---
+++ m2-enum/src/main/java/com/cydeo/review/User.java
@@ -0,0 +1,32 @@
+package com.cydeo.review;
+
+public class User {
+
+    public String username;
+    public String firstname;
+    public String lastname;
+
+    public Role role;
+
+//    public String role = "asdasdd";
+//
+//    List<String> roleList = // "Admin", "Manager", "Employee"
+
+    public User(String username, String firstname, String lastname, Role role) {
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", role=" + role +
+                '}';
+    }
+
+}
